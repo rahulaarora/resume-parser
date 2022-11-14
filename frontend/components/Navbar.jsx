5b6a1a1a83726e133fbca6856b0ef30e63a8cb18
Rahul Arora <rahularora2089@gmail.com>
2022-11-14 22:35:24 +0530

    home button in navigation

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 function Navbar() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
                 <span className="navbar-brand mb-0 h1 fs-3">Resume Parser</span>
+                <span><Link href={"/"} className='link-light text-decoration-none'>Home</Link></span>
             </div>
         </nav>
     );
