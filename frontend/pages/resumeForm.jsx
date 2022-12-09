78632a4e0d9291c41e5fcd80d0a44875b0c16bc7
Rahul Arora <rahularora2089@gmail.com>
2022-12-09 23:55:20 +0530

    Update resumeForm.jsx
    added labels for inputs

--- frontend/pages/resumeForm.jsx
+++ frontend/pages/resumeForm.jsx
@@ -47,44 +47,53 @@ function resumeForm(props) {
                     {/* <input type={"text"} required name="userName" value={user.userName} onChange={changeHandler} /> */}
 
                     <div className="d-flex flex-column">
+                        <label htmlFor="userName">Full Name:</label>
                         <input required type="text" name="userName"
-                            id="name" placeholder="Enter Your Full name*" className="my-2" value={user.userName===null?" ":user.userName} onChange={changeHandler} />
+                            id="name" placeholder="Enter Your Full name*" className="my-2" value={user.userName === null ? " " : user.userName} onChange={changeHandler} />
 
-
+                        <label htmlFor="email">Email:</label>
                         <input required type="email" name="email"
-                            id="name" placeholder="Enter Your Valid Email*" className="my-2" value={user.email===null?" ":user.email} onChange={changeHandler} />
+                            id="name" placeholder="Enter Your Valid Email*" className="my-2" value={user.email === null ? " " : user.email} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Mobile Number:</label>
                         <input type="tel" name="mobileNumber"
-                            id="name" placeholder="Enter Your Mobile Number" className="my-2" value={user.mobileNumber===null?" ":user.mobileNumber} onChange={changeHandler} />
+                            id="name" placeholder="Enter Your Mobile Number" className="my-2" value={user.mobileNumber === null ? " " : user.mobileNumber} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Education:</label>
                         <input type="text" name="education"
-                            id="name" placeholder="Enter your Highest Education" className="my-2" value={user.education===null?" ":user.education} onChange={changeHandler} />
+                            id="name" placeholder="Enter your Highest Education" className="my-2" value={user.education === null ? " " : user.education} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Skills:</label>
                         <input required type="text" name="skills"
-                            id="name" placeholder="Enter your skills separated with comma*" className="my-2" value={user.skills===null?" ":user.skills} onChange={changeHandler} />
+                            id="name" placeholder="Enter your skills separated with comma*" className="my-2" value={user.skills === null ? " " : user.skills} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Company Name:</label>
                         <input type="text" name="companyName"
-                            id="name" placeholder="Enter your current Company Name" className="my-2" value={user.companyName===null?" ":user.companyName} onChange={changeHandler} />
+                            id="name" placeholder="Enter your current Company Name" className="my-2" value={user.companyName === null ? " " : user.companyName} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">College Name:</label>
                         <input type="text" name="collegeName"
-                            id="name" placeholder="Enter your College Name" className="my-2" value={user.collegeName===null?" ":user.collegeName} onChange={changeHandler} />
+                            id="name" placeholder="Enter your College Name" className="my-2" value={user.collegeName === null ? " " : user.collegeName} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Designation:</label>
                         <input type="text" name="designation"
-                            id="name" placeholder="Enter your Designation" className="my-2" value={user.designation===null?" ":user.designation} onChange={changeHandler} />
+                            id="name" placeholder="Enter your Designation" className="my-2" value={user.designation === null ? " " : user.designation} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">Experience:</label>
                         <input type="number" name="experience"
-                            id="text" placeholder="Enter your Experience" className="my-2" value={user.experience===null?" ":user.experience} onChange={changeHandler} />
+                            id="text" placeholder="Enter your Experience" className="my-2" value={user.experience === null ? " " : user.experience} onChange={changeHandler} />
 
 
+                        <label htmlFor="email">LinkedIn URL:</label>
                         <input type="url" name="linkedIn"
-                            id="name" placeholder="Enter the Valid URL" className="my-2" value={user.linkedIn===null?" ":user.linkedIn} onChange={changeHandler} />
+                            id="name" placeholder="Enter the Valid URL" className="my-2" value={user.linkedIn === null ? " " : user.linkedIn} onChange={changeHandler} />
 
 
                     </div>
