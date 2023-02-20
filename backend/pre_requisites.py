60c75920125f8b946f65bcd2c28693977c7ff0ed
Rahul Arora <rahularora2089@gmail.com>
2023-02-20 19:31:45 +0530

    version fixed

--- backend/pre_requisites.py
+++ backend/pre_requisites.py
@@ -1,6 +1,14 @@
 import os
 import nltk
+import ssl
 
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+    
 # Install SpaCy Dependencies
 os.system('python -m pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz')
 
